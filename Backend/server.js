e6b551d08bd55e6aebd3eb64e8cb05f5d083a521
tegar <sumbiang1005@gmail.com>
2026-06-17 00:44:19 +0700

    Fix server.js for Vercel deployment

--- Backend/server.js
+++ Backend/server.js
@@ -29,10 +29,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`)
-    })
-}
+// PERBAIKAN: Hapus if statement
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
 module.exports = app
